c087a4ecd244d414964f2ce91263bcad9d0e24bd
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-02-10 22:15:55 +0800

    Add files via upload

---
+++ gjjh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//#define A 2+2
+//#define B 3+3
+//#define C A*B
+//int main()
+//{
+//	printf("%d\n", C);
+//	return 0;
+//}
+//#define N 4
+//
+//#define Y(n) ((N+2)*n) 
+//int main()
+//{
+//	int z = 2 * (N + Y(5 + 1));
+//	printf("%d\n", z);
+//	return 0;
+//}
+//#define  STRUCT_OFFSET(id, element)  ((unsigned long)(&(( struct id*)0)->element))
+//#define SWAP(n) (((n&0xaaaaaaaa)>>1)+((n&0x55555555)<<1))
